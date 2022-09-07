09c25f9f38b358536588aa66814b1c0cc6c1c5bb
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-07 09:34:42 +0200

    fix .format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for nmb in range(0, 100):
     if nmb == 99:
-        print(f"{nmb:02}")
+        print("{:02d}".format(nmb))
     else:
-        print(f"{nmb:02}", end=", ")
+        print("{:02d}".format(nmb), end=", ")
