efa6574f37266455623566977ad4ce02640cdbf4
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-12 11:23:10 +0200

    copy list without modification

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     copy_list = list(my_list)
     if idx in range(len(my_list)):
-        my_list[idx] = element
+        copy_list[idx] = element
     return (copy_list)
