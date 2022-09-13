c2e8a3fc7bcc8539341972393ee4ee9e53318ef6
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-13 13:29:23 +0200

    delete at idx - case []

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     for size in my_list:
-        if my_list == []:
-            return (my_list)
-        elif idx > 0 or idx > size:
+        if idx >= 0 or idx > size:
             del my_list[idx]
             return (my_list)
         else:
