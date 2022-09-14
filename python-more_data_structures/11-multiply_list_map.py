3dc76ad15351bf4a4ef5a42b4b6c981b676652f9
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-14 13:57:41 +0200

    use map() and lambda() to multiply

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multiply = map(lambda x: x*4, my_list)
-    return (list(multiply))
+    return (list(map(lambda x: x*4, my_list)))
