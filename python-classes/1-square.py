0982a1bd6e71e50f93d232e9b382c0ee5803fd9b
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-19 10:50:21 +0200

    modify class documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,8 +4,8 @@
 
 class Square():
     """ A class Square that defines a square by:
-       private instace attribute: size
-       Instantiation with size (no type/value verification)
+        - Private instace attribute: size
+        - Instantiation with size (no type/value verification)
     """
     def __init__(self, size=0):
         self.__size = size
