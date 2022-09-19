aa9bacb8b8557f2ada86154c17eb8ae12a81fac3
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-19 10:01:47 +0200

    add documentations

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+""" This is where module should be documented """
 class Square:
-    """
-    An empty class to define a square
-    """
+    """ An empty class to define a square """
     pass
