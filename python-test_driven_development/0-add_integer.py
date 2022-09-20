4f5d51559c3c3d645af92baea8dc326bcd1e35ac
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-20 14:20:10 +0200

    doctest

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: 0-add_integer
+
+Add two given numbers: a(int/float) and b(int/float)
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Given two integers and return the sum
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return int(a + b)
