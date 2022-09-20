870952a67316e54a1312a24772ba34e513d38e0d
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-20 14:39:24 +0200

    fix overflow

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if a == None:
+    if a is None:
         raise TypeError("a must be an integer")
 
     if not isinstance(b, int) and not isinstance(b, float):
