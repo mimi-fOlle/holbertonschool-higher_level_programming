005f3b0e11ea5df01e13cf79fee07a28363ecbb6
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-21 12:32:43 +0200

    print name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module: 3-say_my_name
+
+Prints My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print the given string/strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if first_name == None and last_name != None:
+        print("My name is {}".format(last_name))
+
+    if first_name != None and last_name == None:
+        print("My name is {}".format(first_name))
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
