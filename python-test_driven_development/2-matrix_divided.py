28a95322734c46ac487bc372557a1eb0e138662f
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-21 09:38:50 +0200

    add some test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,18 +18,24 @@ def matrix_divided(matrix, div):
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
+    if div == float('inf') or div == float('-inf') or div != div:
+        raise OverflowError("cannot convert float infinity to integer")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
+    
     for row in matrix:
-        if not isinstance(matrix, list):
+        if not isinstance(row, list):
             if not isinstance(list, (int, float)):
-                raise TypeError(Type_err)
-
-        new.append(list(map(lambda x: round(x/div, 2), row)))
-
+                if len(row) == 0 and len(matrix[0]) == 0:
+                    raise TypeError(Type_err)
         if size != len(row):
             raise TypeError(
                   "Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(Type_err)
+
+        new.append(list(map(lambda x: round(x/div, 2), row)))
 
     return (new)
