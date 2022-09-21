79a2bc130baf263db74abc74244c915aad4d8d49
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-21 06:56:01 +0200

    add conditions

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,10 +10,16 @@ def add_integer(a, b=98):
     """
     Given two integers and return the sum
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a + b)
+    if type(a) is float('inf') or type(b) is float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+
+    if type(a) is float('nan') or type(b) is float('nan'):
+        raise ValueError("cannot convert float NaN to integer")
+
+    return int(a + b) 
