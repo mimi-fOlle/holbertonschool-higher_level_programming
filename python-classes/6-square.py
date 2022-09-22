c515c060f429b0ad7ff1589303a2d4186d8d1599
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-22 14:51:16 +0200

    add condition for position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,6 +46,10 @@ class Square():
             if isinstance(value[0], int) and isinstance(value[1], int):
                 if value[0] >= 0 and value[1] >= 0:
                     self.__position = value
+                else:
+                    raise TypeError("position must be a tuple of 2 positive integers")
+            else:
+                raise TypeError("position must be a tuple of 2 positive integers")
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
