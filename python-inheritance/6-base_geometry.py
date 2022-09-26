10a485b138c4f2befe523d9272bbd35cc96c929f
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-26 15:58:21 +0200

    improve geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module: 6-base_geometry """
+
+
+class BaseGeometry:
+    """ a class base on 5-base_geometry.py """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+        return (self.area)
