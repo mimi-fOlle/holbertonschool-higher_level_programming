6b7aea8aacef8fd3620ab2228cc258f2923fedc0
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-26 11:57:47 +0200

    get list of attributes and methods

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This is where the module documentation should be """
+
+
 def lookup(obj):
     """ a function to return the list of available attributes and methods """
     return (dir(obj))
