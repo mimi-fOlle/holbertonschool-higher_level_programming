77ef51745eff29dae26d6336ae1b3e59b6aaa230
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-27 13:59:43 +0200

    load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module: 7-add_item """
+
+
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item(args, filename):
+    my_list = []
+
+    my_list = load_from_json_file(filename)
+
+    for i in args:
+        my_list.append(i)
+    save_to_json_file(my_list, filename)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
