45014d4cb6e652a52d4a1835e803005048a35032
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-27 09:03:00 +0200

    python to json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module: 3-to_json_string """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    To return the JSON representation of an object(string)
+    """
+    return (json.dumps(my_obj))
