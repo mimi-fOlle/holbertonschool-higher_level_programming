25ff41cd7084b3cbd9936a1d250ed4e64faad31a
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-27 10:01:31 +0200

    save to json- dumps vs dump

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     Write an Object to a text file, using a JSON representation
     """
-    js_string = json.dumps(my_obj)
     with open(filename, mode='w') as a_file:
-        a_file.write(js_string)
+        json.dump(my_obj, a_file)
