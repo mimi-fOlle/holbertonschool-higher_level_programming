afea8c0535ee4398af03282ef10fc531494adc56
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-27 09:48:53 +0200

    save to json

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module: 5-save_to_json_file """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Write an Object to a text file, using a JSON representation
+    """
+    js_string = json.dumps(my_obj)
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        print(a_file.write(js_string))
