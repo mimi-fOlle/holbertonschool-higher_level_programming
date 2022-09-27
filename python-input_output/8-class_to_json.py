53d3e585a7e51cbe9f242544434ce9010d15a462
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-27 14:45:26 +0200

    class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module: 8-class_to_json """
+
+
+def class_to_json(obj):
+    """
+    Return the dictionary description with simple data structure
+    for JSON serialization of an object
+    """
+    return (obj.__dict__)
