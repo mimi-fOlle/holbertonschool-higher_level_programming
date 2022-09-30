1ab08411a7cfe0fd654fe48a116003e3855deb8d
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-30 14:42:23 +0200

    load add save 3

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" 
-    Module: 7-add_item (add all args to a Python list and save to a file)
-"""
+""" Module: 7-add_item (add all args to a Python list and save to a file) """
 import json
 import sys
 
