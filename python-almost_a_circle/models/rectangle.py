bb140c586e995cdd8ce8f7411885cdc58ffc1621
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-10-03 13:07:37 +0200

    update rectangle - fix typo

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
-        self.hgith = height
+        self.height = height
         self.x = x
         self.y = y
 
