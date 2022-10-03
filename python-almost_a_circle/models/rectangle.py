57e421e240bc0507a0801f941e7e9014ef235b60
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-10-03 13:05:52 +0200

    update rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,8 +25,8 @@ class Rectangle(Base):
         """ Setting private attribute """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -39,8 +39,8 @@ class Rectangle(Base):
         """ Setting private attribute """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
