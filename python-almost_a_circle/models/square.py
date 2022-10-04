53b6a641c1adcd72fa9a43a91c9785af2e8a695c
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-10-04 09:39:33 +0200

    add size getter and setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,12 @@ class Square(Rectangle):
         """ Return [Square] (<id>) <x>/<y> - <size> """
         return "[Square] ({}) {}/{} - {}".\
                format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
