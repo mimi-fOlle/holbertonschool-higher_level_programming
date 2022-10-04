33cbcfbad231c3662909985372a1e656bb47ac43
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-10-04 13:14:31 +0200

    update base file by adding list_dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ base """
+import json
 
 
 class Base:
@@ -14,3 +15,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Return the JSON string representation of list_dictionaries """
+        if list_dictionaries is None or list_dictionaries is {}:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
