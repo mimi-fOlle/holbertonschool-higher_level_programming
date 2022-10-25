e871dea783858f5870831fbb7ab2160a34600530
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-10-25 13:17:44 +0200

    modify import

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,7 @@
 """ Module: 7-add_item (add all arguments to a list and save to a file) """
 
 
-import json
-import sys
+from sys import argv
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
