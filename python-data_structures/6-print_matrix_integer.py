656b15628c4a8b521ce3003487378f9c8fb097cd
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-02 15:50:10 +0100

    update codes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         j = 0
         for j in range(len(matrix[j])):
             print("{:d}".format(matrix[i][j]), end="")
-            if j < len(matrix[j]) - 1:
-                print(end=" ")
         print()
