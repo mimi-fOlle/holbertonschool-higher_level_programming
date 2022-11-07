72d2e8c2868fa22107a0d37a5788c33f53971ba5
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-07 11:01:58 +0100

    root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the MySQL server user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO user_0d_1@localhost;
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
