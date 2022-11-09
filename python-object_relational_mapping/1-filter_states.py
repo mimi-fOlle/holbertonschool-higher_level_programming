2d6f904d3f19037fc9f9539dd20c5bade6596816
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-09 13:55:54 +0100

    filter big N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,13 +8,14 @@ from sys import argv
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name REGEXP '^[N].*$' ORDER BY states.id")
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
     query_rows = cur.fetchall()
 
     for row in query_rows:
-        print(row)
+        if ('N' in row[1]):
+            print(row)
     cur.close()
     db.close()
