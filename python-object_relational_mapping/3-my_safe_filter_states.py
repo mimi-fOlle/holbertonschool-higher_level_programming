c54642bab5a588bdb0566d5a44c8a2a288223997
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-09 16:50:28 +0100

    sqli- no .format

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name =(%s)\
+                ORDER BY states.id", (argv[4], ))
     query_rows = cur.fetchall()
 
     for rows in query_rows:
