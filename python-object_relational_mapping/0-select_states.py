e1cce536b4d2e36266252eaa7bbce804394a91f6
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-09 08:53:26 +0100

    issue int

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,14 @@
 """ List all states from the database hbtn_0e_0_usa  """
 
 import MySQLdb
+from MySQLdb.constants import FIELD_TYPE
 from sys import argv
 
 
 if __name__ == "__main__":
- 
+    my_conv = { FIELD_TYPE.LONG: int}
     db = MySQLdb.connect(host="localhost", port="3306", user=argv[1], 
-		    passwd=argv[2], db=argv[3])
+		    passwd=argv[2], db=argv[3], conv=my_conv)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
