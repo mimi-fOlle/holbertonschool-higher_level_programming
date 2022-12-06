874031ffa7368fa78135db9fd86c3c54ab287d14
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-06 19:30:18 +0100

    func factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (Number.isNaN(parseInt(n))) {
+    return (1);
+  }
+  if (n === 1) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+
+console.log(factorial(parseInt(process.argv[2])));
