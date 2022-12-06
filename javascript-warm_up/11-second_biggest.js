67fda4b6eea2d39a02902a363653fa7781670895
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-06 21:46:53 +0100

    issue numbers greater than 9

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,8 @@ if (argc < 4) {
   console.log(0);
 } else {
   const arg = process.argv.slice(2);
-  arg.sort().reverse();
-  console.log(arg[1]);
+  arg.sort(function (a, b) {
+    return (a - b);
+  });
+  console.log(arg[arg.length - 2]);
 }
