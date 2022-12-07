7642ba5e3ba7818132cd9e33774013abe8eaec83
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-07 12:38:13 +0100

    add condition

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (this.width > 0 || this.height > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
