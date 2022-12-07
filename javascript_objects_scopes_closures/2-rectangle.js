48abebff34722a2bf7b74f6c900dda84564c3c2b
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-07 12:35:27 +0100

    add condition

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (this.width > 0 || this.height > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
