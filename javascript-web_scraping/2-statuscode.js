429334a14714e16d01f982afccfda7037c4f2cb5
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-12-12 12:17:12 +0100

    status code - debug

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 request.get(process.argv[2], function (error, response, body) {
   if (!error) {
-    console.log('Code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 }
 );
